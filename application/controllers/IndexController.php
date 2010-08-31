1c75a33eb5bb53846f922801553a8343c7410210
Jeduan Cornejo <jeduan@gmail.com>
2010-08-31 03:12:04 -0500

    Funcionalidad de agregar. Forma creada con zf create form Album

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -16,12 +16,30 @@ class IndexController extends Zend_Controller_Action
 
     public function addAction()
     {
-        // action body
+        $form = new Application_Form_Album();
+		$form->submit->setLabel('Add');
+		$this->view->form = $form;
+		
+		if ($this->getRequest()->isPost()) {
+			$formData = $this->getRequest()->getPost();
+			if ($form->isValid($formData)) {
+				$artist = $form->getValue('artist');
+				$title = $form->getValue('title');
+				$albums = new Application_Model_DbTable_Albums();
+				$albums->addAlbum($artist, $title);
+				
+				$this->_helper->redirector('index');
+				
+			} else {
+				$form->populate($formData);
+				
+			}
+		}
     }
 
     public function editAction()
     {
-        // action body
+	
     }
 
     public function deleteAction()
