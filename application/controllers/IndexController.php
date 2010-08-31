f9cd02ae615dd30fd94bb9ab6a38527f2ef7d8f6
Jeduan Cornejo <jeduan@gmail.com>
2010-08-31 03:19:31 -0500

    Funcionalidad de edicion

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -39,12 +39,38 @@ class IndexController extends Zend_Controller_Action
 
     public function editAction()
     {
-	
+        $form = new Application_Form_Album();
+		$form->submit->setLabel('Save');
+		$this->view->form = $form;
+		
+		if ($this->getRequest()->isPost()) {
+			$formData = $this->getRequest()->getPost();
+			if ($form->isValid($formData)) {
+				$artist = $form->getValue('artist');
+				$title = $form->getValue('title');
+				$id = (int)$form->getValue('id');
+								
+				$albums = new Application_Model_DbTable_Albums();
+				$albums->updateAlbum($id, $artist, $title);
+				
+				$this->_helper->redirector('index');
+				
+			} else {
+				$form->populate($formData);
+				
+			}
+		} else {
+			$id = $this->_getParam('id', 0);
+			if ($id > 0) {
+				$albums = new Application_Model_DbTable_Albums();
+				$form->populate($albums->getAlbum($id));
+				
+			}
+		}
     }
 
     public function deleteAction()
     {
-        // action body
     }
 
 
